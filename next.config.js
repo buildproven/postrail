1ed2c0358c3ca602be69ac75081651c038d6318c
Brett Stark <brett@brettstark.com>
2025-12-15 22:02:35 -0600

    chore: restore Sentry config in JS format

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
+const { withSentryConfig } = require('@sentry/nextjs')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
 }
 
-module.exports = nextConfig
+const sentryWebpackPluginOptions = {
+  silent: true,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+}
+
+module.exports =
+  process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+    ? withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+    : nextConfig
